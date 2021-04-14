b5f14f7703f3445a4f87ab9c50a31428998ff6fe
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-04-14 20:40:47 -0300

    05.06_Removendo_Transacao_Atualizando_Pagina_2

--- app/treinaweb-flutter-gerenciador-gastos-pessoais/lib/screens/home/components/body.dart
+++ app/treinaweb-flutter-gerenciador-gastos-pessoais/lib/screens/home/components/body.dart
@@ -13,7 +13,6 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  var refreshKey = GlobalKey<RefreshIndicatorState>();
   ContaService cs = ContaService();
   TransacaoService ts = TransacaoService();
   ContaRestService crs = ContaRestService();
@@ -34,93 +33,91 @@ class _BodyState extends State<Body> {
     return RefreshIndicator(
       onRefresh: _refresh,
       child: Padding(
-        padding: EdgeInsets.only(top: 70),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 175,
-              child: FutureBuilder(
-                future: _loadContas,
+          padding: EdgeInsets.only(top: 70),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 175,
+                child: FutureBuilder(
+                  future: _loadContas,
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.hasData) {
+                      _contas = snapshot.data;
+                      return ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: _contas.length,
+                          padding: EdgeInsets.only(left: 16, right: 8),
+                          itemBuilder: (context, index) {
+                            return cardConta(context, _contas[index]);
+                          }
+                      );
+                    } else {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 24, top: 32, bottom: 16, right: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Últimas transações",
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700,
+                      color: Colors.black),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (_) => TransacaoScreen()
+                            )
+                        );
+                      },
+                      child: Text(
+                        "Ver todas",
+                        style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700,
+                            color: Colors.blue),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              FutureBuilder(
+                future: _loadTransacoes,
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.hasData) {
-                    _contas = snapshot.data;
-                    return ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-                        itemCount: _contas.length,
-                        padding: EdgeInsets.only(left: 16, right: 8),
-                        itemBuilder: (context, index) {
-                          return cardConta(context, _contas[index]);
-                        }
+                    _transacoes = snapshot.data;
+                    return Expanded(
+                      child: ListView.builder(
+                        scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: _transacoes.length > 8 ? 8 : _transacoes.length,
+                          padding: EdgeInsets.all(10),
+                          itemBuilder: (context, index) {
+                          return cardTransacao(context, index, _transacoes[index]);
+                          }
+                      ),
                     );
                   } else {
                     return Center(
                       child: CircularProgressIndicator(),
                     );
                   }
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 24, top: 32, bottom: 16, right: 24),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Últimas transações",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700,
-                    color: Colors.black),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (_) => TransacaoScreen()
-                          )
-                      );
-                    },
-                    child: Text(
-                      "Ver todas",
-                      style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700,
-                          color: Colors.blue),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            FutureBuilder(
-              future: _loadTransacoes,
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  _transacoes = snapshot.data;
-                  return Expanded(
-                    child: ListView.builder(
-                      scrollDirection: Axis.vertical,
-                        shrinkWrap: true,
-                        itemCount: _transacoes.length > 8 ? 8 : _transacoes.length,
-                        padding: EdgeInsets.all(10),
-                        itemBuilder: (context, index) {
-                        return cardTransacao(context, index, _transacoes[index]);
-                        }
-                    ),
-                  );
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
                 }
-              }
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
       ),
     );
   }
 
   Future<Null> _refresh() async {
-    refreshKey.currentState?.show(atTop: false);
-
     setState(() {
       _loadTransacoes = _getTransacoes();
       _loadContas = _getContas();
